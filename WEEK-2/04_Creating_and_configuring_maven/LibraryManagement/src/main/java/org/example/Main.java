a76b513de751776750917f1735e51cb1760be504
lakshmanbhukya <lakshmannaikbhukya17@gmail.com>
2026-06-25 22:45:08 +0530

    feat: add initial implementation of Library Management system with Maven configuration, including application context, service, and repository classes

---
+++ WEEK-2/04_Creating_and_configuring_maven/LibraryManagement/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.example.repository.BookRepository;
+import org.example.service.BookService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main{
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+        BookService service=(BookService) context.getBean("bookService");
+        service.addBook();
+    }
+}
