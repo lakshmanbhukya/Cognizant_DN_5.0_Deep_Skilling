991f842613b803c28f1e9e3eabff9ae410df558d
lakshmanbhukya <lakshmannaikbhukya17@gmail.com>
2026-07-04 14:40:54 +0530

    Add API Gateway and Eureka Discovery Server
    - Created a new API Gateway module with necessary configurations and dependencies in pom.xml.
    - Implemented the main application class for the API Gateway.
    - Added a logging filter to intercept and log requests in the API Gateway.
    - Configured application properties for the API Gateway, including server port and Eureka discovery settings.
    - Added test class for the API Gateway to ensure context loads correctly.
    - Included an image for the Eureka dashboard.
    - Established a new Eureka Discovery Server module with its own pom.xml.
    - Implemented the main application class for the Eureka Discovery Server and enabled Eureka server functionality.
    - Configured application properties for the Eureka Discovery Server, including server port and client settings.
    - Added test class for the Eureka Discovery Server to verify context loads correctly.

---
+++ WEEK-4/Microservices/Microservices_with_API_gateway/eureka-and-api-gateway/Account/src/main/java/org/example/account/restController/AccountController.java
@@ -0,0 +1,37 @@
+package org.example.account.restController;
+
+import org.example.account.model.Account;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/account")
+public class AccountController {
+    public static List<Account> account=new ArrayList<Account>();
+    static {
+        account.add(new Account("00987987973432","savings",234343));
+        account.add(new Account("00987987573432","savings",234345));
+        account.add(new Account("00987987673432","savings",234346));
+        account.add(new Account("00987987473432","savings",234347));
+        account.add(new Account("00987987073432","savings",234348));
+    }
+
+    @GetMapping("/{account_id}")
+    public ResponseEntity<Account> getAccountById(@PathVariable String account_id){
+        for(Account a : account){
+            if(a.getAccount_id().equals(account_id)){
+                return ResponseEntity.ok(a);
+            }
+        }
+        return ResponseEntity.status(HttpStatusCode.valueOf(204)).build();
+    }
+
+
+}
